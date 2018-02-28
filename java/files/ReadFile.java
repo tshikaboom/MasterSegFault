af3bbf7f4183cf78457505bf55cc0c4294e9f86a
Alex <lavigne958@gmail.com>
2018-02-28 15:46:49 +0100

    [add] java file examples

---
+++ java/files/ReadFile.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class ReadFile {
+    public static BufferedReader openFile(String file) throws FileNotFoundException {
+	return new BufferedReader(new FileReader(file));
+    }
+
+    public static String readLine(BufferedReader br) throws IOException {
+	return br.readLine();
+    }
+
+
+    public static void main(String[] args) {
+	BufferedReader bf = null;
+	try {
+	    bf = ReadFile.openFile(args[0]);
+	    
+	    String line;
+	    
+	    while( (line = ReadFile.readLine(bf)) != null) {
+		System.out.println("red: " + line);
+	    }
+	} catch (IOException  e) {
+	    e.printStackTrace();
+	} finally {
+	    try {
+		bf.close();
+	    } catch (Exception e) {
+		e.printStackTrace();
+	    }
+	}
+    }
+}
---
+++ java/files/WriteFile.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class WriteFile {
+    public static BufferedWriter openFile(String file) throws IOException {
+	return new BufferedWriter(new FileWriter(file));
+    }
+
+    public static void writeLine(BufferedWriter bw, String line) throws IOException {
+	bw.write(line);
+    }
+
+    public static void main(String[] args) {
+	BufferedWriter bw = null;
+
+	try {
+	    bw = WriteFile.openFile(args[0]);
+
+	    WriteFile.writeLine(bw, "TEST TEST TEST\nTOTOTOOT");
+	    bw.close();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
+}
