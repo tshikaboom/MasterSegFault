e513404a06f20f3dfcf1674fa7d2f06431409ec6
Mike Rudek <lplit@users.noreply.github.com>
2018-03-01 20:43:44 +0100

    PUUUULLLLL

--- workspace/hashcode/src/hashcode/Main.java
+++ workspace/hashcode/src/hashcode/Main.java
@@ -14,16 +14,19 @@ public class Main {
 			BufferedReader bf = ReadFile.openFile(args[0]);
 			BufferedWriter bw = WriteFile.openFile(args[0].substring(0, args[0].length()-3) + ".out");
 			int col, row, f, n;
-			
-			String[] utils = bf.readLine().split(" ");
+			String ln = bf.readLine();
+			String[] utils = ln.split(" ");
 			col = Integer.parseInt(utils[0]);
 			row = Integer.parseInt(utils[1]);
 			f = Integer.parseInt(utils[2]);
 			n = Integer.parseInt(utils[3]);
 			
-			hashcode.Radom solution = new Radom();
-			String res = solution.compute(col, row, f, n);
-			
+//			hashcode.Radom solution = new Radom();
+			ShortestDistance sd = new ShortestDistance();
+//			String res = solution.compute(col, row, f, n);
+
+            // Eat it line by line
+			String res = sd.compute(ln);
 			System.out.println(res);
 			
 			WriteFile.writeLine(bw, res);
--- workspace/hashcode/src/hashcode/Ride.java
+++ workspace/hashcode/src/hashcode/Ride.java
@@ -1,3 +1,5 @@
+package hashcode;
+
 // ride from [0, 0] to [1, 3], earliest start 2, latest finish 9
 
 public class Ride {
@@ -14,22 +16,30 @@ public class Ride {
 
     public Ride(String line){
         String[] tokens = line.split(" ");
-        this.from_x = tokens[0];
-        this.from_y = tokens[1];
-        this.to_x = tokens[2];
-        this.to_y = tokens[3];
-        this.start = tokens[4];
-        this.deadline = tokens[5];
-        this.id = id+=;
+        this.from_x = Integer.parseInt(tokens[0]);
+        this.from_y = Integer.parseInt(tokens[1]);
+        this.to_x = Integer.parseInt(tokens[2]);
+        this.to_y = Integer.parseInt(tokens[3]);
+        this.start = Integer.parseInt(tokens[4]);
+        this.deadline = Integer.parseInt(tokens[5]);
+        this.id = id++;
         this.laxite = 0;
     }
 
     // Distance between two rides
-    public distance(Ride r) {
+    public int distance(Ride r) {
         int curr_x = r.from_x;
         int curr_y = r.from_y;
         int dst_x = r.to_x;
         int dst_y = r.to_y;
-        return Math.abs(curr_x - dst_x) + Math.abs(curr_y - dst_y)
+        return Math.abs(curr_x - dst_x) + Math.abs(curr_y - dst_y);
+    }
+
+    public String compute(String ln) {
+        return "";
+    }
+
+    public String toString() {
+        return "";
     }
 }
